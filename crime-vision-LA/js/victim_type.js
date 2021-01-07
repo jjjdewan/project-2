feb1f179b74eaff4522eece107b0526ee0c7564f
jjjdewan <jdewan@sbit-us.com>
2021-01-06 20:10:56 -0800

    submit final

---
+++ crime-vision-LA/js/victim_type.js
@@ -0,0 +1,170 @@
+function displayTree() {
+    // main svg
+    var svg = d3.select("div#v-pills-type").select("svg"),
+        width = +svg.attr("width"),
+        height = +svg.attr("height"),
+        g = svg.append("g").attr("transform", "translate(20,0)");       // move right 20px.
+
+    // x-scale and x-axis
+    // var experienceName = ["", "Basic 1.0","Alright 2.0","Handy 3.0","Expert 4.0","Guru 5.0"];
+    // var formatSkillPoints = function (d) {
+    //     return experienceName[d % 6];
+    // }
+    var xScale = d3.scaleLinear()
+        .domain([10000, 20000])
+        .range([0, 350]);
+
+    var xAxis = d3.axisTop()
+        .scale(xScale)
+        .ticks(10)
+    // .tickFormat(formatSkillPoints);
+
+    // Setting up a way to handle the data
+    var tree = d3.cluster()                 // This D3 API method setup the Dendrogram datum position.
+        .size([height, width - 500])    // Total width - bar chart width = Dendrogram chart width
+        .separation(function separate(a, b) {
+            return a.parent == b.parent            // 2 levels tree grouping for category
+                || a.parent.parent == b.parent
+                || a.parent == b.parent.parent ? 0.4 : 0.8;
+        });
+
+    var stratify = d3.stratify()            // This D3 API method gives cvs file flat data array dimensions.
+        .parentId(function (d) { return d.id.substring(0, d.id.lastIndexOf(".")); });
+    //
+    // fetch data from csv file
+    //
+    d3.csv("./data-source/layout.csv", row, function (error, data) {
+        if (error) throw error;
+
+        var root = stratify(data);
+        tree(root);
+
+        // Draw every datum a line connecting to its parent.
+        var link = g.selectAll(".link")
+            .data(root.descendants().slice(1))
+            .enter().append("path")
+            .attr("class", "link")
+            .attr("d", function (d) {
+                return "M" + d.y + "," + d.x
+                    + "C" + (d.parent.y + 100) + "," + d.x
+                    + " " + (d.parent.y + 100) + "," + d.parent.x
+                    + " " + d.parent.y + "," + d.parent.x;
+            });
+
+        // Setup position for every datum; Applying different css classes to parents and leafs.
+        var node = g.selectAll(".node")
+            .data(root.descendants())
+            .enter().append("g")
+            .attr("class", function (d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
+            .attr("transform", function (d) { return "translate(" + d.y + "," + d.x + ")"; });
+
+        // Draw every datum a small circle.
+        node.append("circle")
+            .attr("r", 4);
+
+        // Setup G for every leaf datum.
+        var leafNodeG = g.selectAll(".node--leaf")
+            .append("g")
+            .attr("class", "node--leaf-g")
+            .attr("transform", "translate(" + 8 + "," + -13 + ")");
+
+        leafNodeG.append("rect")
+            .attr("class", "shadow")
+            .style("fill", function (d) { return d.data.color; })
+            .attr("width", 2)
+            .attr("height", 20)
+            .attr("rx", 2)
+            .attr("ry", 2)
+            .transition()
+            .duration(800)
+            .attr("width", function (d) { return xScale(d.data.value); });
+
+        leafNodeG.append("text")
+            .attr("dy", 14.5)
+            .attr("x", 5)
+            .style("text-anchor", "start")
+            .style("font-size", "11px")
+            .text(function (d) {
+                return d.data.id.substring(d.data.id.lastIndexOf(".") + 1).split(" ")[0];
+            });
+
+        // Write down text for every parent datum
+        var internalNode = g.selectAll(".node--internal");
+        internalNode.append("text")
+            .attr("y", -10)
+            .style("text-anchor", "middle")
+            .style("font-size", "11px")
+            .text(function (d) {
+                return d.data.id.substring(d.data.id.lastIndexOf(".") + 1);
+            });
+
+        // Emphasize the y-axis baseline.
+        svg.selectAll(".grid").select("line")
+            .style("stroke-dasharray", "20,1")
+            .style("stroke", "black");
+
+        // The moving ball
+        var ballG = svg.insert("g")
+            .attr("class", "ballG")
+            .attr("transform", "translate(" + 1100 + "," + height / 2 + ")");
+        ballG.insert("circle")
+            .attr("class", "shadow")
+            .style("fill", "steelblue")
+            .attr("r", );
+        ballG.insert("text")
+            .style("text-anchor", "middle")
+            .style("font-size", "10px")
+            .attr("dy", 3);
+
+        // Animation functions for mouse on and off events.
+        d3.selectAll(".node--leaf-g")
+            .on("mouseover", handleMouseOver)
+            .on("mouseout", handleMouseOut);
+
+        function handleMouseOver(d) {
+            tooltip.style("left", d3.event.pageX + 10 + "px");
+            tooltip.style("top", d3.event.pageY - 25 + "px");
+            tooltip.style("display", "inline-block");
+            tooltip.html(d.data.id.substring(d.data.id.lastIndexOf(".") + 1));
+            
+            var leafG = d3.select(this);
+
+            leafG.select("rect")
+                .attr("stroke", "#4D4D4D")
+                .attr("stroke-width", "2");
+
+            var ballGMovement = ballG.transition()
+                .duration(400)
+                .attr("transform", "translate(" + (d.y
+                    + xScale(d.data.value) + 90) + ","
+                + (d.x + 1.5) + ")");
+
+            ballGMovement.select("circle")
+                .style("fill", d.data.color)
+                .attr("r", 18);
+
+            ballGMovement.select("text")
+                .delay(300)
+                .text(d3.format(",")(d.data.value));
+
+            
+        }
+        function handleMouseOut() {
+            var leafG = d3.select(this);
+
+            leafG.select("rect")
+                .attr("stroke-width", "0");
+
+            tooltip.style("display", "none");
+        }
+
+    });
+}
+function row(d) {
+    return {
+        id: d.id,
+        value: +d.value,
+        color: d.color
+    };
+}
+displayTree();
---
+++ crime-vision-SFO/README.md
@@ -0,0 +1,42 @@
+# Crime.ly
+Maps utilizing SF Open Data - Built for Hack Against Violence
+
+## Problem
+Crime is prevalent in the Bay Area, and it is hard to know what neighborhoods are safe to walk around in unless you are from the area. We wanted to create a way for users to be more aware of where crimes occur throughout San Francisco so they can make smarter, safer choices.
+
+## Our Solution
+Utilizing crime reports listed in SF Open Data and Google's Maps API, we created a website with all the crime reports laid out on a map. This allows users to see what areas have higher incidences of crime, and what crimes are committed in those areas.
+
+## Getting Started
+When you open the map, all the crime reports from the past year are displayed across San Francisco. We color-coded the different categories, red representing more dangerous crimes and showing what areas you would want to be most careful in. We added a drop down filter for specific categories so that you can also look at what areas certain crimes occur in.
+
+## Contributing
+Feel free to fork the project and add features or use the code in your own projects.
+
+## License
+Crime.ly is MIT licensed.
+
+## Screenshots
+
+## * Landing Page
+![alt tag](http://i.imgur.com/qGAVNLF.png)
+
+## * Find out crime in your area
+![alt tag](http://i.imgur.com/ZomasTk.png)
+
+## * Filter for specific crimes
+![alt tag](http://i.imgur.com/0xGfh3B.png)
+
+## * Our Team
+![alt tag](http://i.imgur.com/obzWMhj.png)
+
+
+### Team Members
+
+[Nick Russo](https://github.com/sinwav)
+
+[Jason Sutter](https://github.com/jasonsutter87)
+
+[Jesus Garcia](https://github.com/heysusgarcia)
+
+[Simon Lu](https://github.com/Simonl0425)
